ee92904285bf690fd7f66eb90609aebf1143842f
khessss <khess@reagan.com>
2025-10-30 21:15:53 -0500

    Update hittable.py

--- hittable.py
+++ hittable.py
@@ -14,10 +14,6 @@ class Hittable(ABC):
         pass
     
     def normal(self, p, eps=1e-4):
-        '''
-        TODO: change this to spherical
-        computes surface normal at point p
-        '''
         dx = np.array([eps, 0, 0, 0])
         dy = np.array([0, eps, 0, 0])
         dz = np.array([0, 0, eps, 0])
@@ -30,13 +26,14 @@ class Hittable(ABC):
         n = np.array([nx, ny, nz, nw])
         n -= np.dot(n, p) * p #projects gradient onto the tangent space
         
-        return n/np.linalg.norm(n)
+        norm = np.linalg.norm(n)
+        if norm < 1e-8:
+                return np.zeros_like(n)
+        return n / norm
         
 
 
 class Sphere(Hittable):
-    '''Sphere in Spherical space
-    '''
     
     def __init__(self, center, radius, color):
         super().__init__(color)
@@ -54,16 +51,24 @@ class Sphere(Hittable):
         if norm < 1e-8:
             return np.zeros_like(n)
         return n / norm
-        pass
 
 class Cylinder(Hittable):
-    def __init__(self, radius, color):
+    def __init__(self, center, radius, color):
         super().__init__(color)
+        self.center = np.array(center) / np.linalg.norm(center)
         self.radius = radius
     
     def sdf(self, p):
         r = np.sqrt(p[2]**2 + p[3]**2)
         return np.arccos(np.clip(r, -1.0, 1.0)) - self.radius
+    
+    def normal(self, p, eps=1e-4):
+        n = self.center - p
+        n -= np.dot(n, p) * p
+        norm = np.linalg.norm(n)
+        if norm < 1e-8:
+            return np.zeros_like(n)
+        return n / norm
 
 class Half_space(Hittable):
 
